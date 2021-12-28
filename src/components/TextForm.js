6b0d07c63c51e8f5d3e72ad87afebc124bd13f46
parijatmitra <parijatmitra20@gmail.com>
2021-12-28 20:55:06 +0530

    1 file modified

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,10 +6,10 @@ export default function TextForm(props){
     <>
 <div>
   
-<div class="mb-3">
+<div className="mb-3">
   <h1>{props.heading}</h1>
-<label for="myBox" class="form-label">{props.heading}</label>
-<textarea class="form-control" id="myBox" rows="8"></textarea>
+
+<textarea className="form-control" id="myBox" rows="8"></textarea>
 </div>
 </div>
     </>
