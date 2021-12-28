d3820b74047addef5803ea899721c5969f38311c
parijatmitra <parijatmitra20@gmail.com>
2021-12-28 21:37:38 +0530

    done with day2

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,7 +3,18 @@ import PropTypes from 'prop-types'
 
 
 export default function TextForm(props){
+  const handleUpConvert = ()=>{
+    // console.log("Upper Case was called");
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+
+  const handleOnChange = (event)=>{
+    // console.log("On change");
+    setText(event.target.value);
+  }
   const [text, setText] = useState('Enter Text here');
+
   return (
     <>
 <div>
@@ -11,9 +22,9 @@ export default function TextForm(props){
 <div className="mb-3">
   <h1>{props.heading}</h1>
 
-<textarea className="form-control" value= {text} id="myBox" rows="8"></textarea>
+<textarea className="form-control" value= {text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className= "btn btn-primary">Convert to Uppercase</button>
+<button className= "btn btn-primary" onClick = {handleUpConvert}>Convert to Uppercase</button>
 </div>
     </>
   )
