5b108ff5d76ee4acb20a75e83c9e35634caa99ba
parijatmitra <parijatmitra20@gmail.com>
2021-12-28 20:45:49 +0530

    adding another components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./style.css";
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm'
 
 
 export default function App() {
   return (
     <>
   <Navbar title = "TextUtils" aboutText = "About TextUtils"/>
+  <div className = "container">
+  <TextForm/>
+  </div>
   
     </>
 
