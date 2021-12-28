66a2769496b061fb8512e860ee40f1c7310353d3
parijatmitra <parijatmitra20@gmail.com>
2021-12-28 21:14:36 +0530

    changes in textbox state

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
+
 export default function TextForm(props){
+  const [text, setText] = useState('Enter Text here');
   return (
     <>
 <div>
@@ -9,7 +11,7 @@ export default function TextForm(props){
 <div className="mb-3">
   <h1>{props.heading}</h1>
 
-<textarea className="form-control" id="myBox" rows="8"></textarea>
+<textarea className="form-control" value= {text} id="myBox" rows="8"></textarea>
 </div>
 <button className= "btn btn-primary">Convert to Uppercase</button>
 </div>
