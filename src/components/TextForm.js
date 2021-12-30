0a3e870c87942cfc116d8bcc28d8b9721a2aea69
parijatmitra <parijatmitra20@gmail.com>
2021-12-30 20:39:14 +0530

    adding more changes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,6 +9,12 @@ export default function TextForm(props){
     setText(newText);
   }
 
+  const handleLowConvert = ()=>{
+    // console.log("Upper Case was called");
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
   const handleOnChange = (event)=>{
     // console.log("On change");
     setText(event.target.value);
@@ -17,15 +23,24 @@ export default function TextForm(props){
 
   return (
     <>
-<div>
-  
+<div className = "container">
+<h1>{props.heading}</h1>
 <div className="mb-3">
-  <h1>{props.heading}</h1>
+  
 
 <textarea className="form-control" value= {text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className= "btn btn-primary" onClick = {handleUpConvert}>Convert to Uppercase</button>
+<button className= "btn btn-primary mx-2" onClick = {handleUpConvert}>Convert to Uppercase</button>
+<button className= "btn btn-primary mx-2" onClick = {handleLowConvert}>Convert to Lowercase</button>
 </div>
+
+<div className = "container my-3">
+  <h1> Your text summary</h1>
+  <p>{text.split(" ").length} words and {text.length} characters</p>
+  <p>{0.008 * text.split(" ").length} minutes to read</p>
+  <h3>Preview</h3>
+  <p>{text}</p>
+  </div>
     </>
   )
 }
